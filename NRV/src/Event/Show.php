b183e4305db8e6232a046554400b827bf2ddd881
Cobap-B <Chugo.rochedreux@gmail.com>
2024-11-06 17:35:58 +0100

    ajout Show et Action

--- NRV/src/Event/Show.php
+++ NRV/src/Event/Show.php
@@ -1,1 +1,29 @@
 <?php
+namespace NRV\Event;
+
+class Show{
+
+    private int $id;
+    private string $category;
+    private string $title;
+    private string $artist;
+    private int $duration;
+    private string $description;
+    private string $fileName;
+
+    public function __construct(string $id, string $category, string $title, string $artist, int $duration, string $description, string $fileName){
+        $this->id = $id;
+        $this->category = $category;
+        $this->title = $title;
+        $this->artist = $artist;
+        $this->duration = $duration;
+        $this->description = $description;
+        $this->fileName = $fileName;
+    } 
+
+    public function  __get(string $at):mixed{
+        if (property_exists ($this, $at)) return $this->$at;
+        throw new \NRV\exception\InvalidPropertyNameExcepetion ("$at: invalid proprety");
+    }
+
+}
---
+++ NRV/src/Exception/InvalidPropertyNameExcepetion.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+namespace NRV\exception;
+
+class InvalidPropertyNameExcepetion extends \Exception{
+
+} 
