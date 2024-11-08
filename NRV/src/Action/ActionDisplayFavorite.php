cd91cc83ce4e441984bf16c3fc0f2bd106633e8e
Kevin Ngo <kevin.ngo8@etu.univ-lorraine.fr>
2024-11-08 17:01:24 +0100

    modif actionDisplayFavorite (à finir)

--- NRV/src/Action/ActionDisplayFavorite.php
+++ NRV/src/Action/ActionDisplayFavorite.php
@@ -1,1 +1,50 @@
 <?php
+
+namespace NRV\action;
+
+use NRV\Auth\AuthnProvider;
+use NRV\Repository\FestivalRepository;
+
+class ActionDisplayFavorite extends Action{
+
+    public function execute(): string
+    {
+        if (isset($_SESSION['userIdx'])) {
+            if ($this->http_method === 'GET') {
+                return <<<END
+                <form method="POST" action="?action=display-favorite">
+                <button type="submit"> Afficher préference </button>
+                </form>
+            END;
+            } else {
+                $html = "<div>Affichage des préférences</div>";
+                $id = filter_var($_POST['id']);
+                if (AuthnProvider::authenticate($_SESSION['userId'], $id)) {
+                    $r = FestivalRepository::getInstance();
+
+                    return $html;
+                } else {
+                    return "<div>Pas de Favori</div>";
+                }
+            }
+            return $html;
+        }
+        if ($this->http_method === 'GET') {
+            return <<<END
+                <form method="POST" action="?action=display-favorite">
+                <button type="submit"> Afficher préference </button>
+                </form>
+            END;
+        }else {
+            $html = "<div>Affichage des préférences</div>";
+            $id = filter_var($_POST['id']);
+            if (AuthnProvider::authenticate($_SESSION['userId'], $id)) {
+                $r = FestivalRepository::getInstance();
+
+                return $html;
+            } else {
+                return "<div>Pas de Favori</div>";
+            }
+        }
+    }
+}
