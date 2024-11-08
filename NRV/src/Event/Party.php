4b27f5d3bec8490bd4d3dd96e49a93c51a38a995
Cobap-B <Chugo.rochedreux@gmail.com>
2024-11-08 14:34:02 +0100

    ajout img et audio pour show

--- NRV/src/Event/Party.php
+++ NRV/src/Event/Party.php
@@ -7,11 +7,13 @@ class Party extends Event{
 
     private int $place;
     private array $shows;
+    private int $tarif;
 
-    public function __construct(string $id, string $name, string $dateDebut, string $dateFin, int $place, array $shows = []){
+    public function __construct(string $id, string $name, string $dateDebut, string $dateFin, int $place, int $tarif, array $shows = []){
         parent::__construct($id, $tname, $dateDebut, $dateFin);
         $this->place = $place;
         $this->shows = $shows;
+        $this->tarif = $tarif;
     } 
 
    
