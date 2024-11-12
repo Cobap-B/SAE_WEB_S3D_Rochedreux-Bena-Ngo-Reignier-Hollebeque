54ca0ae89cc6a39390d4ff28952aac7bb2441125
Kevin Ngo <kevin.ngo8@etu.univ-lorraine.fr>
2024-11-12 16:28:52 +0100

    modif ActionDefault

--- NRV/src/Action/ActionDefault.php
+++ NRV/src/Action/ActionDefault.php
@@ -4,9 +4,38 @@ namespace NRV\action;
 class ActionDefault extends Action {
 
     public function execute() {
-        return <<<FIN
-            <div>Bienvenue</div>
+        $html = "";
+
+        $pdo = \NRV\Repository\FestivalRepository::makeConnection();
+        $html .= <<<FIN
+                    <div>
+                        <section>
+                            <h2>Party</h2>
         FIN;
-        
+        $party = $pdo->displayParty();
+        foreach ((Array)$party as $h){
+            $render = new \NRV\Renderer\PartyRenderer($h);
+            $html .= $render->render(2);
+            $html .= "<br><br>";
+        }
+
+        $html .= <<<FIN
+                        </section>
+                        <section>
+                            <h2>Show</h2>
+        FIN;
+
+        $shows = $pdo->displayShow($cate, $date, $lieu);
+        foreach($shows as $a){
+            $render = new \NRV\Renderer\ShowRenderer($a);
+            $html .= $render->render(2);
+            $html .= "<br><br>";
+        }
+        $html .= <<<FIN
+                        </section>
+                    </div>
+        FIN;
+
+        return $html;
     }
 }
