ab2102945514807ab1a7985711f948c418142519
Hugo Bena <benahugo93@gmail.com>
2024-11-12 14:52:02 +0100

    ajout de l'action display party

---
+++ NRV/src/Action/ActionDisplayParty.php
@@ -0,0 +1,22 @@
+<?php
+namespace NRV\Action;
+
+class ActionDisplayParty extends Action {
+
+    public function execute(): string
+    {
+        $html = "";
+
+        $pdo = \NRV\Repository\FestivalRepository::makeConnection();
+
+            $party = $pdo->displayParty();
+            foreach ((Array)$party as $h){
+                $render = new \NRV\Renderer\PartyRenderer($h);
+                $html .= $render->render(2);
+                $html .= "<br><br>";
+            }
+            return $html;
+
+    }
+
+}
